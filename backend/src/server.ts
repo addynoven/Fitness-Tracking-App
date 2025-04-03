c67de02602b5cdcfacadc151ab218df5d22be227
Aditya Sahu (Neon) <dmcbaditya@gmail.com>
2025-04-03 23:23:41 +0530

    first commit

---
+++ backend/src/server.ts
@@ -0,0 +1,13 @@
+import express from "express";
+
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+	res.send("Fitness Tracking API is running!");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+	console.log(`Server is running on http://localhost:${PORT}`);
+});
